d27c04b382c04cd28be410d55f7210aedd2c47b5
hswiergiel <h.swiergiel@hotmail.fr>
2023-12-30 18:07:41 +0100

    initial commit

--- Eagle.java
+++ Eagle.java
@@ -1,4 +1,4 @@
-public class Eagle extends Bird {
+public class Eagle extends Bird implements Fly {
 
     private boolean flying;
     private int altitude;
@@ -20,5 +20,53 @@ public class Eagle extends Bird {
     @Override
     public String sing() {
         return "Screech!";
+    }
+
+    @Override
+    public void takeOff(){
+        if(!this.flying && this.altitude == 0){
+            this.flying = true;
+            System.out.printf("%s takes off in the sky.%n", this.getName());
+        }
+    }
+
+    @Override
+    public void glide(){
+        if(this.flying && this.altitude > 0){
+            System.out.printf("%s glides into the air.%n", this.getName());
+        }
+    }
+
+    @Override
+    public int ascend(int meters){
+        if(this.flying){
+            this.altitude = this.altitude + meters;
+            System.out.printf("%s flies upward, altitude: %d%n", this.getName(), this.altitude);
+        }
+        return this.altitude;
+    }
+
+    @Override
+    public int descend(int meters){
+        if (this.flying && this.altitude > 0){
+            this.altitude = Math.max(this.altitude - meters, 1);
+            System.out.printf("%s flies downward, altitude: %d%n", this.getName(), this.altitude);
+        }
+        return this.altitude;
+    }
+
+    @Override
+    public void land(){
+        if (this.flying){
+            if(altitude == 1){
+                this.flying = false;
+                this.altitude = 0;
+                System.out.printf("%s lands on the ground.%n", this.getName());
+            } else {
+                System.out.printf("%s is too high, can't land.%n", this.getName());
+            }
+        } else {
+            System.out.printf("%s is not actually flying so it doesn't have to land.%n", this.getName());
+        }
     }
 }
---
+++ Fly.java
@@ -0,0 +1,11 @@
+public interface Fly {
+    void takeOff();
+
+    void glide();
+
+    int ascend (int meters);
+
+    int descend (int meters);
+
+    void land();
+}
